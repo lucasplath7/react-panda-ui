88c51d85a90ec56b02b82ca0dc40a2dd9260f4e2
Lucas Plath <lucas.plath@gmail.com>
2023-07-24 21:35:11 -0400

    stop force renewal of certs

--- src/.config.js
+++ src/.config.js
@@ -1,5 +1,5 @@
 console.log('PORT: ', process.env.PORT)
 module.exports = {
     //apiURL: 'http://react-panda.com:5000/api',
-    apiURL: 'http://www.react-panda.com:5000/api',
+    apiURL: 'http://www.react-panda.com:5001/api',
 }
