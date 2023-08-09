3c07c4afd1833862f5d364f095faa185e05d49d3
Lucas Plath <lucas.plath@gmail.com>
2023-08-09 16:07:12 -0400

    troubleshoot env vars in build step

--- src/.config.js
+++ src/.config.js
@@ -1,5 +1,8 @@
-console.log('API URL: ', process.env.REACT_APP_API_URL)
+const apiURL = process.env.REACT_APP_API_URL || 'https://www.react-panda.com:5001/api'
+
+console.log('API URL: ', apiURL)
+
 module.exports = {
     //apiURL: 'http://localhost:5000/api',
-    apiURL: process.env.REACT_APP_API_URL,
+    apiURL: apiURL,
 }
